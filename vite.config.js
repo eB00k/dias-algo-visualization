8d0d0241866078e36042e2d9a6ed19d5bc42c204
Dastan Akatov <107542637+eB00k@users.noreply.github.com>
2024-04-23 16:53:44 +0600

    added deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  base: "/dias-algo-visualization",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
